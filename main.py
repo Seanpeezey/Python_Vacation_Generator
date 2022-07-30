c93454ab00cba083708f8c0b9eb644ee52fca2b8
Sean Perinuzzi <sperinuzzi@gmail.com>
2022-07-30 14:20:37 -0400

    Main.py file was created. Lists containing destinations, as well as sub lists that contain restaurants, transportation & entertainment have been added. git status

---
+++ main.py
@@ -0,0 +1,21 @@
+#This list will supply the randomly generated vacation destinations.
+ 
+destination_list = ["New York City", "St Augustine, Florida","Boston"]
+
+#These next three lists will pertain to our fist destination on our list New York City.
+
+nyc_restaurant_list = ["Carmine's Italian Restaurant", "STK Steakhouse Midtown NYC","La Grande Boucherie"]
+nyc_transportation_list = ["Taxi Cab", "Subway", "Uber XL", "Horse & Carriage","Bike Carriage"]
+nyc_entertainment_list = ["Broadway", "Top of The Rock: Observation Deck", "Madame Tussauds New York","250th Rooftop Bar & Grill"]
+
+#These next three lists will pertain to our second destination. St Augustine, Florida.
+
+florida_restaurant_list = ["Salt Life", "Harry's Seafood, Bar & Grille", "Columbia Restaurant", "Prohibition Kitchen"]
+florida_transportation_list =["Old Town Trolley Tours St Augustine", "Taxi Cab", "Salty Seas Transportation"]
+florida_entertainment_list = ["Castillo de San Marcos National Monument", "Ripley's Believe It or Not!", "Ponce de Leon's Fountain of Youth Archaeological Park","Lightner Museum"]
+
+#These three lists will pertain to our final destination. Boston.
+
+boston_restaurant_list = ["Del Frisco's Double Eagle Steakhouse", "Bostonia Public House", "Buttermilk & Bourbon"]
+boston_transportation_list =["Uber Xl", "Taxi Cab", "Bus"]
+boston_entertainment_list =["Boston Crime Tour", "Samuel Adams Brewery Tour", "Fenway Park", "Whale Watching boat tour", "Boston Aquarium"]
